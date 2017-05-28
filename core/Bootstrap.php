37c390f88f3fe668b4be8943cd04dd8df2c49325
Massimo Biagioli <biagiolimassimo@gmail.com>
2017-05-28 10:11:38 +0200

    Aggiunto ErrorHandler

--- core/Bootstrap.php
+++ core/Bootstrap.php
@@ -2,6 +2,11 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
+// Register ErrorHandler
+$whoops = new \Whoops\Run;
+$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+$whoops->register();
+
 // Load .env
 $dotenv = new Dotenv\Dotenv(__DIR__ . '/..');
 $dotenv->load();
