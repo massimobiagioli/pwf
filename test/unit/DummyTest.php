e5710ec0af056b6503975fde2e335329f9705a88
Massimo Biagioli <biagiolimassimo@gmail.com>
2017-05-28 09:33:57 +0200

    first commit

---
+++ test/unit/DummyTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Test\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class DummyTest extends TestCase {
+    
+    public function testTrueIsTrue() {
+        $this->assertTrue(true);
+    }
+        
+}
